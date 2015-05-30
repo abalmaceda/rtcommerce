7e74cec75a0aa5a735b92875b5d340879e4b43f3
Alvaro Balmaceda <oravla@gmail.com>
2015-05-30 19:10:31 -0300

    init proyect info

--- client/rtcommerce.js
+++ client/rtcommerce.js
@@ -1,4 +1,5 @@
-if (Meteor.isClient) {
+/*i
+f (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
@@ -21,3 +22,4 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+*/
